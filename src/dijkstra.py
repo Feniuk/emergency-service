cdfe1ce1403c49fa648365d58bf4c1f6cd206379
Feniuk <143640539+Feniuk@users.noreply.github.com>
2026-03-22 16:18:16 +0100

    logic changed

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -5,13 +5,22 @@ import heapq
 def dijkstra(graph, starting_point, destination):
     infinity = sys.maxsize
 
-    initial_data = {"A":{"cost": infinity, "previous":[]},
-                    "B":{"cost": infinity, "previous":[]},
-                    "C":{"cost": infinity, "previous":[]},
-                    "D":{"cost": infinity, "previous":[]},
-                    "E":{"cost": infinity, "previous":[]},
-                    "F":{"cost": infinity, "previous":[]},
-                    "G":{"cost": infinity, "previous":[]},
-                    "H":{"cost": infinity, "previous":[]},
-                    "I":{"cost": infinity, "previous":[]}}
+    cost = {}
+    for node in graph:
+        cost[node] = infinity
+
+    previous = {}
+    for node in graph:
+        previous[node] = None
+
+    # initial_data = {"A":{"cost": infinity, "previous":[]},
+    #                 "B":{"cost": infinity, "previous":[]},
+    #                 "C":{"cost": infinity, "previous":[]},
+    #                 "D":{"cost": infinity, "previous":[]},
+    #                 "E":{"cost": infinity, "previous":[]},
+    #                 "F":{"cost": infinity, "previous":[]},
+    #                 "G":{"cost": infinity, "previous":[]},
+    #                 "H":{"cost": infinity, "previous":[]},
+    #                 "I":{"cost": infinity, "previous":[]}}
+    
     visited = []
