a4eabd6c9b15008560f47d51a455583d95986a00
Feniuk <143640539+Feniuk@users.noreply.github.com>
2026-03-24 12:57:37 +0100

    half way

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -5,22 +5,25 @@ import heapq
 def dijkstra(graph, starting_point, destination):
     infinity = sys.maxsize
 
-    cost = {}
-    for node in graph:
-        cost[node] = infinity
-
-    previous = {}
-    for node in graph:
-        previous[node] = None
-
-    # initial_data = {"A":{"cost": infinity, "previous":[]},
-    #                 "B":{"cost": infinity, "previous":[]},
-    #                 "C":{"cost": infinity, "previous":[]},
-    #                 "D":{"cost": infinity, "previous":[]},
-    #                 "E":{"cost": infinity, "previous":[]},
-    #                 "F":{"cost": infinity, "previous":[]},
-    #                 "G":{"cost": infinity, "previous":[]},
-    #                 "H":{"cost": infinity, "previous":[]},
-    #                 "I":{"cost": infinity, "previous":[]}}
+    initial_data = {"1":{"cost": infinity, "previous":[]},
+                    "2":{"cost": infinity, "previous":[]},
+                    "3":{"cost": infinity, "previous":[]},
+                    "4":{"cost": infinity, "previous":[]},
+                    "5":{"cost": infinity, "previous":[]},
+                    "6":{"cost": infinity, "previous":[]},
+                    "7":{"cost": infinity, "previous":[]},
+                    "8":{"cost": infinity, "previous":[]},
+                    "9":{"cost": infinity, "previous":[]}}
     
+    initial_data[starting_point]["cost"] = 0
     visited = []
+    current = starting_point
+    for punkt in range(9):
+        if current not in visited:
+            visited.append(current)
+            min_heap = []
+            for neighbor_node in graph[current]:
+                if neighbor_node not in visited:
+                    cost = initial_data[current]["cost"] + graph[current][neighbor_node]
+                    if cost < initial_data[current]["cost"]:
+                        initial_data[current]["cost"] = cost
