d0fdc1d22fcb1dfba6981581f2bd767624ceb5e1
Feniuk <143640539+Feniuk@users.noreply.github.com>
2026-03-24 14:21:24 +0100

    dijkstra done

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -1,6 +1,6 @@
 from data import graph, hospitals
 import sys
-from heapq import heappush, heapify
+from heapq import heappush, heapify, heappop
 
 def dijkstra(graph, starting_point, destination):
     infinity = sys.maxsize
@@ -18,10 +18,11 @@ def dijkstra(graph, starting_point, destination):
     initial_data[starting_point]["cost"] = 0
     visited = []
     current = starting_point
+    min_heap = []
+    heappush(min_heap, (0, starting_point))
     for punkt in range(9):
         if current not in visited:
             visited.append(current)
-            min_heap = []
             for neighbor_node in graph[current]:
                 if neighbor_node not in visited:
                     cost = initial_data[current]["cost"] + graph[current][neighbor_node]
@@ -29,12 +30,11 @@ def dijkstra(graph, starting_point, destination):
                         initial_data[neighbor_node]["cost"] = cost
                         initial_data[neighbor_node]["previous"] = initial_data[current]["previous"] + [current]
                     heappush(min_heap, (initial_data[neighbor_node]["cost"], neighbor_node))
-                    print(min_heap)
-                    print(heapify(min_heap))
-                    current = min_heap[0][1]
-                    print(current)
+                    
+        temp = heappop(min_heap)
+        current = temp[1]
 
-        print("Shortest Distance: " + str(initial_data[destination]["cost"]))
-        print("Shortest Path: " + str(initial_data[destination]["previous"] + list(destination)))
+    print("Shortest Distance: " + str(initial_data[destination]["cost"]))
+    print("Shortest Path: " + str(initial_data[destination]["previous"] + [destination]))
 
 dijkstra(graph, "Hbf", "University")
